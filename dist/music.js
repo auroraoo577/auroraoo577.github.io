27c6fe440e2c1657c09680ce0bb51ccbd3b6f367
auroraoo577 <2817269704@qq.com>
2021-04-16 14:51:34 +0800

    Site updated: 2021-04-16 14:51:28

---
+++ dist/music.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    lrcType: 3,
+    audio: [
+      {
+        name: "瞳で抱いてて",
+        artist: '石井明美',
+        url: 'http://music.163.com/song?id=567889&userid=561163091',
+        cover: ' ',
+        lrc: ' '
+      },
+    ]
+});
